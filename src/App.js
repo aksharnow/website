a5ea3a5f68e8c46859e763ebc107e3b40fcc88c6
Akshar Now <aksharnow@gmail.com>
2018-06-22 13:13:23 +0530

    build update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React, { Component } from 'react'
 
-
-class Product extends React.Component {
-  render(){
-    return(
-      <div className="product">
-            <div className="product-image">
-                <img src={'https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/broccoli.jpg'}/>
-            </div>
-            <h4 className="product-name">{"Oranges"}</h4>
-            <h4 className="product-price">$ {"10"}</h4>
-            <div className="product-action">
-                <button className="added" type="button"> Added </button>
-            </div>
-      </div>
-    )
-  }
-}
-
 class App extends React.Component{
   render(){
     return(
       <div className="products">
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+        <div className="product">
+          <h1>Akshar Now</h1>
+          <p> The new training sessions will start on June 29th </p>
+          <p> Please reach out to <a href={"mailto:aksharnow@gmail.com?subject=More details about the training&body=Hello, %0D%0A I'm interested in the training, please let me know more details"}>aksharnow@gmail.com</a> for more details </p>
         </div>
+      </div>
     );
   }
 
